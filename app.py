e7c84d96c3a0ab4a9641b47010b387a96228c188
Brian Murray <murraycoding@gmail.com>
2025-02-08 08:48:20 -0500

    added template for app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+"""
+File to act as the entry point for the application.
+"""
+
+# establish the directory paths for the folders where the files will live
+
+# generate the text files from the pdf statements
+
+# process the statements
+
+# write the data to a spreadseheet
